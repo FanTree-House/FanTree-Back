f830ad10de5b2fdf789b04a9ec8d82fcbd1fdd1a
silicao3o <zergskybmw@gmail.com>
2024-07-22 12:38:00 +0900

    init data

---
+++ src/main/java/com/example/fantreehouse/FanTreeHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantreehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanTreeHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanTreeHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fantreehouse/FanTreeHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantreehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanTreeHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
