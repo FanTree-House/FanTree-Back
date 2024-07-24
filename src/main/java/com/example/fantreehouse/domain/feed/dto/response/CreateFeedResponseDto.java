b521cf305245377d1c3343bf5b003de50adfa86d
MonGroy <mongtualmr@gmail.com>
2024-07-24 20:55:23 +0900

    chore: delete unnecessary class, update dto #4

--- src/main/java/com/example/fantreehouse/domain/feed/dto/response/CreateFeedResponseDto.java
+++ src/main/java/com/example/fantreehouse/domain/feed/dto/response/CreateFeedResponseDto.java
@@ -16,6 +16,7 @@ public class CreateFeedResponseDto {
         CreateFeedResponseDto responseDto = new CreateFeedResponseDto();
         responseDto.contents = newFeed.getContents();
         responseDto.post_picture = newFeed.getPost_picture();
+        responseDto.likeCount = 0;
         return responseDto;
     }
 }
