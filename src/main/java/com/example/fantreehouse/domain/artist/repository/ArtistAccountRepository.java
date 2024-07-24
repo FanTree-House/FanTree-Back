cddd366966523349be79e3c745b9db02836efbc9
MonGroy <mongtualmr@gmail.com>
2024-07-24 21:09:03 +0900

    fix: 머지 후 오류 수정 #4

--- src/main/java/com/example/fantreehouse/domain/artist/repository/ArtistAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fantreehouse.domain.artist.repository;
-
-import com.example.fantreehouse.domain.artist.entity.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
