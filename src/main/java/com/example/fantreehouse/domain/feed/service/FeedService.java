41d4c85966099a3ffeee2d9b75cc2b6345a90b50
MonGroy <mongtualmr@gmail.com>
2024-07-24 21:47:06 +0900

    chore : 주석정리 #4

--- src/main/java/com/example/fantreehouse/domain/feed/service/FeedService.java
+++ src/main/java/com/example/fantreehouse/domain/feed/service/FeedService.java
@@ -14,6 +14,7 @@ import com.example.fantreehouse.domain.user.entity.User;
 import com.example.fantreehouse.domain.user.entity.UserRoleEnum;
 import com.example.fantreehouse.domain.user.entity.UserStatusEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,22 +31,18 @@ public class FeedService {
 
     /**
      * Feed 생성
-     * @param group_name
+     * @param groupName
      * @param userDetails
      * @param file
      * @param requestDto
      * @return
      */
-    public CreateFeedResponseDto createFeed(String group_name, UserDetailsImpl userDetails, MultipartFile file, CreateFeedRequestDto requestDto) {
+    public CreateFeedResponseDto createFeed(String groupName, UserDetailsImpl userDetails, MultipartFile file, CreateFeedRequestDto requestDto) {
 
         User loginUser = userDetails.getUser();
-
-        //유저 status 확인 (활동 여부)
         checkUserStatus(loginUser.getStatus());
-        //아티스트인지 확인
         checkUserRole(loginUser.getUserRole());
-        //url 의 group_name 과 loginUser 의 소속 group 명 이 동일한지 확인
-        checkArtistGroup(loginUser.getArtist().getArtistGroup().getGroupName(), group_name);
+        checkArtistGroup(loginUser.getArtist().getArtistGroup().getGroupName(), groupName);
 
         //file 경로 추출
 //        if (file != null && file.isEmpty()) {
@@ -53,11 +50,10 @@ public class FeedService {
 //            filePath = fileDirPath
 //        }
 
-//        Feed newFeed = new Feed.of(requestDto, loginUser, group_name, filePath);
+//        Feed newFeed = new Feed.of(requestDto, loginUser, groupName, filePath);
         Feed newFeed = Feed.of(requestDto, loginUser, loginUser.getArtist().getArtistGroup());//file 기능 전 임시 사용
 
         feedRepository.save(newFeed);
-
         return CreateFeedResponseDto.of(newFeed);
     }
 
@@ -65,16 +61,13 @@ public class FeedService {
     public UpdateFeedResponseDto updateFeed(String group_name, Long artistFeedId, UserDetailsImpl userDetails, MultipartFile file, UpdateFeedRequestDto requestDto) {
 
         User loginUser = userDetails.getUser();
-        //유저 status 확인 (활동여부)
         checkUserStatus(loginUser.getStatus());
 
         //요청하는 feed 찾기
         Feed foundFeed = feedRepository.findById(artistFeedId)
                 .orElseThrow(() -> new NotFoundException(FEED_NOT_FOUND));
 
-        //feed 가 작성된 그룹과 입력받은 그룹 이름이 같은지 확인
         checkArtistGroup(foundFeed.getUser().getArtist().getArtistGroup().getGroupName(), group_name);
-        //로그인 유저와 feed 작성 유저가 동일한지 확인
         checkWriter(loginUser.getId(), foundFeed.getUser().getId());
 
 //        //파일 경로 추출 후, updateFeed 매개변수로 넣기 (file 은 임시)
@@ -82,7 +75,6 @@ public class FeedService {
 
 //        Feed updatedFeed = foundFeed.updateFeed(requestDto, filePath);
         Feed updatedFeed = foundFeed.updateFeed(requestDto); //file 기능 전 임시 사용
-
         return UpdateFeedResponseDto.of(updatedFeed);
     }
 
@@ -96,22 +88,24 @@ public class FeedService {
     public FeedResponseDto getFeed(String groupName, Long artistFeedId, UserDetailsImpl userDetails) {
 
         User loginUser = userDetails.getUser();
-        //유저 status 확인 (활동여부)
         checkUserStatus(loginUser.getStatus());
 
         //요청하는 feed 찾기
         Feed foundFeed = feedRepository.findById(artistFeedId)
                 .orElseThrow(() -> new NotFoundException(FEED_NOT_FOUND));
 
-        //찾는 feed 가 작성된 그룹과 입력받은 그룹 이름이 같은지 확인
         checkArtistGroup(foundFeed.getUser().getArtist().getArtistGroup().getGroupName(), groupName);
-
         return FeedResponseDto.of(foundFeed);
     }
 
 
     //Feed 다건 조회(페이지)
-    public FeedResponseDto getAllFeed(String groupName, UserDetailsImpl userDetails) {
+    public FeedResponseDto getAllFeed(String groupName, UserDetailsImpl userDetails, Pageable pageable) {
+
+
+
+
+
         return null;
     }
 
@@ -130,7 +124,6 @@ public class FeedService {
         User loginUser = userDetails.getUser();
         UserRoleEnum loginUserRole = loginUser.getUserRole();
 
-        //유저 status 확인 (활동여부)
         checkUserStatus(loginUser.getStatus());
 
         //권한이 Artist 이거나 Entertainment 여야 함
@@ -149,7 +142,7 @@ public class FeedService {
 //        //loginUser 의 그룹이름이 groupName 과 동일한지..확인..? //엔터인 경우는 그 그룹이름을 가진 그룹을 갖는지 확인..?
 //        loginUser.getArtist().getArtistGroup().getGroupName()
 
-        // 즉, 로그인유저가 '작성자 본인'이거나 '작성자의 엔터테인먼트를 가진 유저'인 경우
+        // 로그인유저가 '작성자 본인'이거나 '작성자의 엔터테인먼트를 가진 유저'인 경우
         if (! (loginUserId.equals(feedWriterId) ||
                 loginUser.getEntertainment().getUser().getId().equals(loginUserId))) {
             throw new AuthorizedException(UNAUTHORIZED);
@@ -157,24 +150,27 @@ public class FeedService {
 
         feedRepository.delete(foundFeed);
     }
+
+    //유저 status 확인 (활동 여부)
     private void checkUserStatus(UserStatusEnum userStatusEnum) {
         if (!userStatusEnum.equals(UserStatusEnum.ACTIVE_USER)) {
             throw new AuthorizedException(UNAUTHORIZED);
         }
     }
 
+    //login 유저가 아티스트인지 확인
     private void checkUserRole(UserRoleEnum userRoleEnum) {
         if (!userRoleEnum.equals(UserRoleEnum.ARTIST)) {
             throw new AuthorizedException(UNAUTHORIZED);
         }
     }
-
+    //url 의 groupName 과 loginUser 의 소속 group 명 이 동일한지 확인
     private void checkArtistGroup(String groupName, String group_name) {
         if (!groupName.equals(group_name)) {
             throw new AuthorizedException(UNAUTHORIZED);
         }
     }
-
+    //로그인 유저와 feed 작성 유저가 동일한지 확인
     private void checkWriter(Long loginUserId, Long feedWriterId) {
         if (!loginUserId.equals(feedWriterId)) {
             throw new AuthorizedException(UNAUTHORIZED);
