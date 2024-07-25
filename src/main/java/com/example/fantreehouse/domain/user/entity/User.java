ab72a5c39fac4ae9161dca75cf35b74052fd1cca
silicao3o <zergskybmw@gmail.com>
2024-07-26 01:17:14 +0900

    [Feat] 카카오 로그인 DB 연동 #3

--- src/main/java/com/example/fantreehouse/domain/user/entity/User.java
+++ src/main/java/com/example/fantreehouse/domain/user/entity/User.java
@@ -100,6 +100,7 @@ public class User extends Timestamped {
         return refreshToken == null ? true : false;
     }
 
+
     public void saveRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
