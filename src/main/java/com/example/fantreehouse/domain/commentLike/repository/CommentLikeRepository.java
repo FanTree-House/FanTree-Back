36d72293285c574b83cc7f6370d8bbfe1537eb0d
Woojin Kim <rladnwlsok@naver.com>
2024-08-14 22:26:56 +0900

    [Fix] 댓글 좋아요 기능 추가 및 수정

--- src/main/java/com/example/fantreehouse/domain/commentLike/repository/CommentLikeRepository.java
+++ src/main/java/com/example/fantreehouse/domain/commentLike/repository/CommentLikeRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
     List<CommentLike> findAllCommentLikeByCommentId(Long commentId);
+
+    Optional<CommentLike> findByUserIdAndCommentId(Long uesrId, Long artistFeedCommentId);
+
+    Long countByCommentId(Long artistFeedCommentId);
 }
